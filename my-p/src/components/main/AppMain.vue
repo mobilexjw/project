878c4a46f65f7fddf32303a0590a4b78e5112312
zj <1052843688@qq.com>
2017-12-26 20:51:49 +0800

    create vue-cli of my project

---
+++ my-p/src/components/main/AppMain.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+	<header>
+		shjgj kjh 
+	</header>
+	<section>
+          <ul>
+          	<li></li>
+          	<li></li>
+          	<li></li>
+          </ul>		
+	</section>
+	<nav>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</nav>
+	<div class="list">
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+	<footer>
+		<ul><li></li><li></li></ul>
+	</footer>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"AppMain",
+	}
+</script>
+
+<style lang="scss">
+   $basecolor:red;
+    body{
+    	background:$basecolor;
+    }
+</style>
